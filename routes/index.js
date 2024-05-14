d4a1a531dbd3e59f2aad03b9f02dad01819694cf
Simi oluwatomi <deifilius1@gmail.com>
2024-05-14 22:27:15 +0100

    feat: :card_file_box: get all posts
    seed the database using the seeder, add a database client and get all posts on the index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const database = require('../config/database');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
+router.get('/', async function(req, res, next) {
+  try {
+    const db = await database;
+
+    const results = await db.collection('bloggy').find({}).toArray()
+
+    res.json(results)
+
+  } catch (error) {
+    console.log(`Error querying database: ${error}`);
+
+    res.status(500).json({error: `Internal server error`});
+  }
 });
 
 module.exports = router;
